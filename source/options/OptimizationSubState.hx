1df0a2fb8f97f6beb2c4c05da4e452d4fd9d3721
Andre Nicholas Jr <151701402+Andrhevn23Rbx@users.noreply.github.com>
2025-07-15 18:37:39 -0500

    Update OptimizationSubState.hx
    e

--- source/options/OptimizationSubState.hx
+++ source/options/OptimizationSubState.hx
@@ -4,7 +4,7 @@ class OptimizationSubState extends BaseOptionsMenu
 {
 	public function new()
 	{
-		title = 'Optmise Settings';
+		title = 'Optimization';
 		rpcTitle = 'Optimization Settings Menu'; //for Discord Rich Presence
 
 		//I'd suggest using "Chars & BG" as an example for making your own option since it is the simplest here
